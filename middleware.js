8db3110e38b60d9cc12205ba9f566228366fa190
zachhaggar15-coder <zach.haggar15@gmail.com>
2026-06-20 15:34:17 +0100

    Fix homepage tracking redirect loop

--- middleware.js
+++ middleware.js
@@ -8,11 +8,10 @@ export default function middleware(request) {
   const url = new URL(request.url);
 
   if (ROOT_TRACKING_PARAMS.some(param => url.searchParams.has(param))) {
-    return new Response(null, {
-      status: 308,
-      headers: {
-        Location: '/',
-      },
-    });
+    url.search = '';
+    url.hash = '';
+    url.hostname = 'www.mealprep.org.uk';
+    url.protocol = 'https:';
+    return Response.redirect(url, 308);
   }
 }
